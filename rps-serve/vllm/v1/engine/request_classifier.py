348b72afc625863c5a0720f8c68bb8cd4afe98a6
Konstantinos Papaioannou <konstantinos.papaioannou@imdea.org>
2026-09-23 12:17:23 +0200

    Add classifiers
    Signed-off-by: Konstantinos Papaioannou <konstantinos.papaioannou@imdea.org>

--- rps-serve/vllm/v1/engine/request_classifier.py
+++ rps-serve/vllm/v1/engine/request_classifier.py
@@ -124,7 +124,8 @@ class SmartRequestClassifier(BaseRequestClassifier):
 
         if model_name in self.cache:
             entry = self.cache[model_name]
-            classifiers_path = (
+            classifiers_path = os.path.join(os.path.dirname(os.getcwd()), "../artifacts/classifiers")
+            (
                 "/home/konstantinos.papaioannou/rps-serve/artifacts/classifiers"
             )
             path = os.path.join(classifiers_path, f"{model_name}.pkl")
